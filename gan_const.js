d3e995e6b7d991a635be25a548644b70a2681d30
ngovanthinh13 <65067636+ngovanthinh13@users.noreply.github.com>
2021-05-13 06:38:29 +0700

    Delete gan_const.js
    file thua

--- gan_const.js
+++
@@ -1,9 +0,0 @@
-
-// Thay đổi phần tử trong mảng
-const COLORS = ["Xanh", "Đỏ", "Tím", "Vàng"];
-console.log(COLORS[0]); // Xanh
-
-// Gán lại giá trị cho phần tử đầu tiên
-COLORS[0] = "Hồng";
-console.log(COLORS[0]); // Hồng
-console.log(colors(...COLORS))
